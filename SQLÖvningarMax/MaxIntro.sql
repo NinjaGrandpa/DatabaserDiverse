3f0f0309a8fbf5b80069ff8c2def9f5c308ee746
NinjaGrandpa <34811547+NinjaGrandpa@users.noreply.github.com>
2022-11-22 16:23:47 +0100

    Update MaxIntro.sql

--- SQLÖvningarMax/MaxIntro.sql
+++ SQLÖvningarMax/MaxIntro.sql
@@ -5,11 +5,21 @@
 
 USE KlassenDb;
 
-CREATE TABLE KlassenTbl 
-(
-Id int Primary Key,
-Förnamn nvarchar(max) not null,
-Efternamn nvarchar(max) not null,
-Ålder int,
-Hemort nvarchar(max) not null
-);
+--CREATE TABLE KlassenTbl 
+--(
+--Id int Primary Key,
+--Förnamn nvarchar(max) not null,
+--Efternamn nvarchar(max) not null,
+--Ålder int,
+--Hemort nvarchar(max) not null
+--);
+
+--INSERT INTO KlassenTbl ([Id], [Förnamn], [Efternamn], [Ålder], [Hemort])
+--Values 
+--(1,'Max','Jenslöv',20,'Mölndal'),
+--(2,'Jonathan','Long',20,'Vrångö'),
+--(3,'Daniel','Petrén',20,'Mölndal'),
+--(4,'Carl','Kennedal',20,'Mölndal'),
+--(5,'Erik','Fredenval',20,'Mölndal');
+
+SELECT * FROM KlassenTbl;
